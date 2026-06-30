fd9d128c61c8e542c5164f1d82dd3a1e5f33ecea
Enzo <enzoaraujocampos01@gmail.com>
2026-06-30 14:16:20 -0300

    add new sites

--- app/page.tsx
+++ app/page.tsx
@@ -22,15 +22,25 @@ export default function Home() {
     <h2 className="text-4xl font-bold text-white text-center" id="portfolio"><br></br>ALGUNS DOS<br></br>MEUS TRABALHOS<br></br><br></br></h2>
     <div className="flex flex-col items-center lg:bg-[url('/images/portfolio.png')] bg-[url('/images/sm-background.png')] lg:lg-contain bg-contain lg:bg-repeat-x bg-no-repeat pt-12">
       <div className="lg:flex lg:flex-row lg:w-[1000px] xl:w-[1200px] gap-5 bg-no-repeat">
+        <Smartway img="./images/site institucional hidrico quimica.png" siteUrl='https://hidrico-institucional.vercel.app/' title="Institucional - Hídrico Química" description="ㅤ" category="Química / Indústria"/>
         <Smartway siteUrl='https://smartwaycs.com.br' img ='./images/smartway.png' title="Institucional - Smartway" description="ㅤ" category="Indústria"/>
         <Smartway siteUrl='https://taedarquitetura.com.br' img="./images/taed.png" title="Institucional - Taed Arquitetura" description="ㅤ" category="Arquitetura"/>
-        <Smartway siteUrl='https://campinas-aopharmaceutico.com.br' img="./images/campinas.png" title="Institucional - Ao Pharmacêutico" description="Em parceria com @iblancomarketing" category="Farmácia"/>
         </div>
       <div className="lg:flex lg:flex-row lg:w-[1000px] xl:w-[1200px] gap-5 bg-no-repeat">
-        <Smartway img="./images/fisioclinic.png" siteUrl='https://fisioclinicsantos.com.br' title="Institucional - FisioClinic" description="Em parceria com @iblancomarketing" category="Indústria"/>
-        <Smartway img="./images/bailinho.png" siteUrl='https://bailinhomara.com.br' title="Landing Page - Bailinho" description="ㅤ" category="Eventos"/>
-        <Smartway img="./images/skania.png" siteUrl='https://vernizskania.com.br' title="E-commerce - Verniz Skania" description="Em parceria com @iblancomarketing" category="Limpeza"/>
+        <Smartway siteUrl='https://campinas-aopharmaceutico.com.br' img="./images/campinas.png" title="Institucional - Ao Pharmacêutico" description="Em parceria com @iblancomarketing" category="Farmácia"/>
+        <Smartway img="./images/dashboard financeiro integrado com enpoint de CRM.png" siteUrl='#' title="Dashboard - Financeiro integrado com CRM" description="ㅤ" category="Fintech"/>
+        <Smartway img="./images/Agente de IA para Whatsapp + dashboard administrativo.png" siteUrl='#' title="Agente de IA para WhatsApp + Dashboard" description="ㅤ" category="Inteligência Artificial"/>
         </div>
+      <div className="lg:flex lg:flex-row lg:w-[1000px] xl:w-[1200px] gap-5 bg-no-repeat">
+        <Smartway img="./images/skania.png" siteUrl='https://vernizskania.com.br' title="E-commerce - Verniz Skania" description="Em parceria com @iblancomarketing" category="Limpeza"/>
+        <Smartway img="./images/landing page ery santos tricologista.png" siteUrl='https://lp.erysantostricologista.com.br/' title="Landing Page - Tricologista" description="ㅤ" category="Saúde"/>
+        <Smartway img="./images/LAnding page para campanha game changer.png" siteUrl='https://gamechanger.reliveclinic.com.br/' title="Landing Page - Game Changer" description="ㅤ" category="Saúde / Estética"/>
+      </div>
+      <div className="lg:flex lg:flex-row lg:w-[1000px] xl:w-[1200px] gap-5 bg-no-repeat">
+        <Smartway img="./images/landing page exibindo lancamentos de madeira.png" siteUrl='https://lancamentos.grupoindusparquet.com.br/' title="Landing Page - Grupo Indusparquet" description="ㅤ" category="Construção / Decoração"/>
+        <Smartway img="./images/fisioclinic.png" siteUrl='https://fisioclinicsantos.com.br' title="Institucional - FisioClinic" description="Em parceria com @iblancomarketing" category="Saúde"/>
+        <Smartway img="./images/bailinho.png" siteUrl='https://bailinhomara.com.br' title="Landing Page - Bailinho" description="ㅤ" category="Eventos"/>
+      </div>
     </div>
   </>
   );
